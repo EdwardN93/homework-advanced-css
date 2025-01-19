c2a4e01d218fbf05ced151b11d19c6c1480fe570
Edward <110572334+EdwardN93@users.noreply.github.com>
2025-01-19 14:25:04 +0200

    complete project

---
+++ Read Me.md
@@ -0,0 +1,3 @@
+# Pizza menu
+
+Greatest Pizza Menu Ever
---
+++ index.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "/pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+console.log(pizzaData);
+
+const menu = document.querySelector(".pizzas");
+
+pizzaData.forEach((el) => {
+  const html = `
+    <li class="pizza ${el.soldOut ? "sold-out" : ""}">
+            <img src="${el.photoName}" alt="${el.photoName}" />
+            <div>
+              <h3>${el.name}</h3>
+              <p>${el.ingredients}</p>
+              <span>${el.soldOut ? "SOLD OUT" : el.price + "€"}</span>
+            </div>
+    </li>
+
+  `;
+  menu.insertAdjacentHTML("beforeend", html);
+});
